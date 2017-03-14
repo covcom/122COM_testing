913aa0573d8ca2bec684d5d755ac2125a2a3e75b
david <dscroft@gmail.com>
2017-03-14 11:55:25 +0000

    Auto commit

--- lab_queue.h
+++ lab_queue.h
@@ -2,15 +2,14 @@
 #define QUEUE_H
 
 #include <exception>
-#include <memory>
+#include <vector>
 using namespace std;
 
 class Queue
 {
 private:
     const int maxSize;
-    unique_ptr<char[]> queue;
-    int items;
+    vector<char> queue;
 
 public:
     class Full: public exception
@@ -18,7 +17,7 @@ public:
     public:
         virtual const char* what() const throw()
           {
-            return "queue is full";
+            return "Queue is full";
           }
     };
 
@@ -27,16 +26,14 @@ public:
     public:
         virtual const char* what() const throw()
           {
-            return "queue is empty";
+            return "Queue is empty";
           }
     };
 
     /** Initialise the queue, maxSize is the maximum number of
         values that can be stored in the queue at any time **/
-    Queue( const int _maxSize ) : maxSize(_maxSize),
-                                  queue( new char[_maxSize] )
+    Queue( const int _maxSize ) : maxSize(_maxSize)
     {
-        items = 0;
     }    
 
     ~Queue()
@@ -45,9 +42,10 @@ public:
 
     /** Returns the number of values currently stored in the 
         queue **/
-    int num_items()
+    int num_items() const
     {
-        return items;
+        // COMPLETE ME
+        return 0;
     }
 
     /** Add value to the front of the queue, raises Queue::Full 
@@ -56,8 +54,7 @@ public:
     {
         if( num_items() < maxSize )
         {
-            queue[ items ] = value;
-            items += 1;
+            queue.emplace_back(value);
         }
         else
         {
@@ -68,11 +65,11 @@ public:
     /** Returns the value currently stored at the front of the 
         queue, raises Queue::Empty exception is queue is 
         empty **/
-    char front()
+    char front() const
     {
         if( num_items() > 0 )
         {
-            return queue[0];
+            return queue.front();
         }
         else
         {
@@ -83,16 +80,10 @@ public:
     /** Returns the value currently stored at the back of the 
         queue, raises Queue.Empty exception if the queue is
         empty **/
-    char back()
+    char back() const
     {
-        if( num_items() > 0 )
-        {
-            return queue[items-1];
-        }
-        else
-        {
-            throw Empty();
-        }
+        // COMPLETE ME
+        return 0;
     }
 
     /** Removes and returns the value currently stored at the
@@ -100,23 +91,8 @@ public:
         is empty **/
     char pop()
     {
-        if( num_items() > 0 )
-        {
-            char value = queue[0];            // get the value at the front of the queue
-
-            for( int i=0; i<items-1; ++i )    // shuffle everything forward one space
-            {
-                queue[i] = queue[i+1];    
-            }
-
-            items -= 1;                        // reduce the item count by 1
-
-            return value;                    // return the value that was at the front
-        }
-        else
-        {
-            throw Empty();
-        }
+        // COMPLETE ME
+        return 0;
     }
 };
 
--- lab_stack.h
+++ lab_stack.h
@@ -1,42 +1,39 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include <memory>
 #include <exception>
+#include <vector>
 using namespace std;
 
 class Stack
 {
 private:
     const int maxSize;
-    unique_ptr<char[]> stack;
-    int pos;
+    vector<char> stack;
 
 public:
     class Full: public exception
     {
     public:
         virtual const char* what() const throw()
-          {
+        {
             return "Stack is full";
-          }
+        }
     };
 
     class Empty: public exception
     {
     public:
         virtual const char* what() const throw()
-          {
+        {
             return "Stack is empty";
-          }
+        }
     };
 
     /** Initialise the stack, maxSize is the maximum number of
-        values that can be stored in the stack at any time **/
-    Stack( const int _maxSize ) : maxSize(_maxSize), 
-                                  stack( new char[_maxSize] )
+        values that can be stored in the stack at any time */
+    Stack( const int _maxSize ) : maxSize(_maxSize)
     {
-        pos = 0;
     }    
 
     ~Stack()
@@ -44,20 +41,20 @@ public:
     }
 
     /** Returns the number of values currently stored in the 
-        stack **/
-    int num_items()
+        stack */
+    int num_items() const
     {
-        return pos;
+        // REPLACE ME
+        return 0;
     }
 
     /** Add value to the top of the stack, raises Stack::Full 
-        exception is stack is full **/
+        exception if stack is full */
     void push( char value )
     {
         if( num_items() < maxSize )
         {
-            stack[ pos ] = value;
-            pos += 1;
+            stack.emplace_back( value );
         }
         else
         {
@@ -66,34 +63,17 @@ public:
     }
 
     /** Returns the value currently stored at the top of the 
-        stack, raises Stack::Empty exception is stack is 
-        empty **/
+        stack, raises Stack::Empty exception if stack is 
+        empty */
     char top()
     {
-        if( num_items() > 0 )
-        {
-            return stack[ pos ];
-        }
-        else
-        {
-            throw Empty();
-        }
     }
 
     /** Removes and returns the value currently stored at the
-        top of the stack, raises Stack::Empty exception is stack
-        is empty **/
+        top of the stack, raises Stack::Empty exception if stack
+        is empty */
     char pop()
     {
-        if( num_items() > 0 )
-        {
-            pos -= 1;
-            return stack[ pos ];
-        }
-        else
-        {
-            throw Empty();
-        }
     }
 };
 
